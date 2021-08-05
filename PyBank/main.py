5305766d936b844d07850ba76bbc56c9943fe88b
Jake <jakewmayfield@gmail.com>
2021-08-04 20:55:03 -0500

    updated main code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,13 +1,20 @@
+#import dependencies
 import csv
 import os
+
+#variables for iterating & calculations
+total_months=0
+net_profit_loss=0
+current_month=0
+previous_month=0
+profit_loss_change=0
+
+
 
 #get csv file path and make a variable
 from pathlib import Path
 
 budget_data = Path('/Users/Jake/DataClass/python-challenge/PyBank/Resources/budget_data.csv')
-
-#set net_income counter to 0
-net_income=0
 
 #read csv file and print rows
 with open(budget_data, 'r') as csv_file:
@@ -23,11 +30,11 @@ with open(budget_data, 'r') as csv_file:
 
     #calculate net income
     for row in csv_reader:
-        net_income = net_income + int(row[1])
-    print(net_income)
+        net_profit_loss = net_profit_loss + int(row[1])
+    print(net_profit_loss)
     
     #count total rows without header
-    total_months=len(list(csv_reader))-1
+    total_months=len(list(csv_reader))
     print(total_months)
 
 
